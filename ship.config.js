64fd3cee2c5073f83ed2e93e26a8e30f7db57385
azu <azuciao@gmail.com>
2020-06-10 23:24:54 +0900

    fix(shipjs): major update

--- ship.config.js
+++ ship.config.js
@@ -1,1 +1,8 @@
-module.exports = { buildCommand: () => null };
+const semver = require("semver");
+module.exports = {
+    buildCommand: () => null,
+    getNextVersion: ({ revisionRange, commitTitles, commitBodies, currentVersion, dir }) => {
+        // always major update
+        return semver.inc(currentVersion, "major"); 
+    }
+};
